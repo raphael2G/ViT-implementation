96855d3f66633cbc4b05a889aca814704893eea6
Derek <darmfield2023@gmail.com>
2022-10-20 12:19:57 -0400

    Created model charts for all ViT blocks

--- javascript/test.js
+++ javascript/test.js
@@ -1,4 +1,6 @@
 // define unsupported layers as custom js layers
+
+// Gelu Activation Function
 class gelu extends tf.layers.Layer {
     static className = 'gelu';
  
@@ -7,23 +9,72 @@ class gelu extends tf.layers.Layer {
     }
 
     call(features) {
-      // return 0.5 * features * 
-      // (1.0 + Math.tanh(0.7978845608028654 * 
-      //   (features + 0.044715 * 
-      //   Math.pow(features, 3))))
+      // need to implement gelu from the paper 
       return features
     }
  }
  tf.serialization.registerClass(gelu);
 
 
-//  const data_augmentation = await tf.loadLayersModel('../savedModels/ViT/tfjs/data_augmentation/model.json');
-const subModel = await tf.loadLayersModel('../savedModels/ViT/tfjs/subModel/model.json');
-subModel.summary()
+// Patch Encoder (adding position vector to patch embeddings)
+class PatchesEncoder extends tf.layers.Layer {
+  static className = 'PatchesEncoder';
 
-const dummy_input = tf.ones([10, 49, 8])
-const output = subModel.predict(dummy_input)
-output.print()
+  constructor(config) {
+    super(config);
+    
+  }
+
+  call(patches) {
+    const projection = tf.layers.dense({units: projection_dim});
+    const position_embedding = tf.layers.embedding({inputDim: n_patches, outputDim: projection_dim});
+
+    let positions = tf.range(0, n_patches, 1);
+
+
+    let embedded_position = position_embedding.apply(positions);
+    const encoded_patches = projection.apply(patches);
+
+    embedded_positions = tf.reshape(embedded_position, [1, 49, 1]);
+
+
+    const repeatLayer = tf.layers.repeatVector({n: 10, inputShape: [49, 1]});
+    const formatedPositions = repeatLayer.apply(repeatLayer);
+
+    console.log(formatedPositions.shape);
+    console.log(encoded_patches.shape);
+
+    const addLayer = tf.layers.add();
+    return addLayer.apply([embedded_position, encoded_patches]);
+  }
+
+  getClassName() { return 'PatchesEncoder'; }
+}
+
+// 
+const patchEncoder = new PatchesEncoder();
+const subModel = await tf.loadLayersModel('../savedModels/ViT/tfjs/subModel/model.json');
+
+
+function getPrediction(flattenedPatches){
+  const encodedPatches = patchEncoder.apply(flattenedPatches);
+  const output = subModel.apply(encodedPatches);
+
+  encodedPatches.print();
+  output.print();
+  
+  return output
+}
+
+const dummy_data = tf.ones([10, 49, 8]);
+
+let output = getPrediction(dummy_data);
+console.log('output done well')
+
+
+
+
+
 
 
 
